9c0efa0824efc8696b758387f86a96decd70b360
ulyssetsd <utassidis@gmail.com>
2020-04-05 17:46:36 +0100

    #3 create simple database migration folder

--- database/202004051744_init.sql
+++ database/202004051744_init.sql
@@ -1,8 +1,3 @@
-CREATE DATABASE bigbrains;
-
-DROP TABLE users CASCADE;
-DROP TABLE login CASCADE;
-
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
@@ -16,5 +11,3 @@ CREATE TABLE login (
     hash VARCHAR(100) NOT NULL,
     users_id INTEGER REFERENCES users (id) ON DELETE CASCADE
 );
-
-select * from users join login l on users.id = l.users_id;
